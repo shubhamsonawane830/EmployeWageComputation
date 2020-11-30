be44d501f908d4bb1360acca5ea3e5cb821cce59
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-11-30 21:13:15 +0530

    Refactored: BasicToClassLevelMethod

--- EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
+++ EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
@@ -1,21 +1,41 @@
 public class EmployeeWageProblem{
-	public static void main(String args[]){
-		int is_full_time = 1;
-		int is_part_time = 2;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HR = 20;
+	public static final int MAX_WORKINGHRS_PER_MONTH = 100;
+	public static final int MAX_WORKINGDAYS_PER_MONTH = 20;
+	public static int HrsPerDay;
+	public static int Day;
+	public static int DailyWage;
+	public static int MonthlyWage=0;
+	public static int WorkingHrs=0;
 
-		int WagePerHr = 20;
-		int HrsPerDay_FullTime = 8;
-		int HrsPerDay_PartTime = 4;
-		int DailyWage;
+	public static void empHrs(){
+		double EmpCheck = Math.floor(Math.random() * 10) % 3;
+		int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
 
-		int Max_WorkingHrsPerMonth = 100;
-		int Max_WorkingDaysPerMonth = 20;
-		int MonthlyWage = 0;
-		int WorkingHrs = 0;
+		switch(CheckEmp)
+		{
+			case 0:
+				System.out.println("Day: "+ Day +" "+ "Employee is Absent");
+				HrsPerDay=0;
+				break;
+			case 1:
+				System.out.println("Day: "+ Day +" "+"FullTime Employee is Present");
+				HrsPerDay=8;
+				break;
+			case 2:
+				System.out.println("Day: "+ Day +" "+"Part Time Employee is Present");
+				HrsPerDay=4;
+				break;
+			default:
+				System.out.println("Invalid");
+				break;
+		}
+	}
 
-		System.out.println("*****WELCOME TO EMPLOYEE WAGE PROBLEM*****");
-
-		for(int i=1; i<= Max_WorkingDaysPerMonth; i++)
+	public static void monthlywage(){
+		for(Day=1; Day<=MAX_WORKINGDAYS_PER_MONTH; Day++)
 		{
 			if(WorkingHrs>100)
 			{
@@ -24,42 +44,21 @@ public class EmployeeWageProblem{
 			}
 			if(WorkingHrs<=100)
 			{
-				double EmpCheck = Math.floor(Math.random() * 10) % 3;
-				int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
-
-				switch(CheckEmp)
-				{
-					case 0:
-						System.out.println("Employee is Absent");
-						System.out.println("Wage per day is 0");
-						System.out.println("Total Salary till date is "+MonthlyWage);
-						System.out.println();
-						break;
-					case 1:
-						System.out.println("FullTime Employee is Present");
-						DailyWage = WagePerHr * HrsPerDay_FullTime;
-						System.out.println("Wage per day is "+DailyWage);
-						MonthlyWage = MonthlyWage + DailyWage;
-						WorkingHrs = WorkingHrs + HrsPerDay_FullTime;
-						System.out.println("Total Salary till date is "+MonthlyWage);
-						System.out.println();
-						break;
-					case 2:
-						System.out.println("Part Time Employee is Present");
-						DailyWage = WagePerHr * HrsPerDay_PartTime;
-						System.out.println("Wage per Day is "+DailyWage);
-						MonthlyWage = MonthlyWage + DailyWage;
-						WorkingHrs = WorkingHrs + HrsPerDay_PartTime;
-						System.out.println("Total Salary till date is "+MonthlyWage);
-						System.out.println();
-						break;
-					default:
-						System.out.println("Invalid");
-						break;
-				}
+				empHrs();
+				DailyWage = WAGE_PER_HR * HrsPerDay;
+				System.out.println("Salary for day : "+DailyWage);
+				MonthlyWage += DailyWage;
+				System.out.println("Total Salary till date : "+MonthlyWage);
+				WorkingHrs = WorkingHrs + HrsPerDay;
+				System.out.println();
 			}
 		}
+	}
 
+	public static void main(String args[])
+	{
+		System.out.println("*****WELCOME TO EMPLOYEE WAGE PROBLEM*****");
+		monthlywage();
 		System.out.println("Total Salary for month is " +MonthlyWage);
 		System.out.println("Total Working Hours are " +WorkingHrs);
 	}
