b09912ac26adfb8d62749b3fd083ba51fa9f5075
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-11-30 20:42:43 +0530

    Added: DailyWage

--- EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
+++ EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
@@ -1,10 +1,16 @@
 public class EmployeeWageProblem{
 	public static void main(String args[]){
+		int WagePerHr=20;
+		int HrsPerDay=8;
+		int DailyWage;
 		System.out.println("*****WELCOME TO EMPLOYEE WAGE PROBLEM*****");
 		double EmpCheck = Math.floor(Math.random() * 10) % 2;
-		if(EmpCheck == 1)
-			System.out.println("Employee is present");
-		else
+		if(EmpCheck == 1){
+			System.out.println("Employee is Present");
+			DailyWage = WagePerHr*HrsPerDay;
+			System.out.println("Daily Wage is "+DailyWage);
+		}else{
 			System.out.println("Employee is absent");
+		}
 	}
 }
