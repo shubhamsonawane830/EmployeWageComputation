5a4726e1d070a304d5a40406d68a960bfea2db0c
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-11-30 21:09:25 +0530

    Modified: ForWorkingHourPerDay

--- EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
+++ EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
@@ -8,36 +8,59 @@ public class EmployeeWageProblem{
 		int HrsPerDay_PartTime = 4;
 		int DailyWage;
 
-		int WorkingDaysPerMonth = 20;
-		int MonthlyWage;
+		int Max_WorkingHrsPerMonth = 100;
+		int Max_WorkingDaysPerMonth = 20;
+		int MonthlyWage = 0;
+		int WorkingHrs = 0;
 
 		System.out.println("*****WELCOME TO EMPLOYEE WAGE PROBLEM*****");
 
-		double EmpCheck = Math.floor(Math.random() * 10) % 3;
-		int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
-
-		switch(CheckEmp)
+		for(int i=1; i<= Max_WorkingDaysPerMonth; i++)
 		{
-			case 0:
-				System.out.println("Employee is Absent");
-				break;
-			case 1:
-				System.out.println("FullTime Employee is Present");
-				DailyWage = WagePerHr * HrsPerDay_FullTime;
-				System.out.println("Daily Wage is "+DailyWage);
-				MonthlyWage = DailyWage * WorkingDaysPerMonth;
-				System.out.println("Monthly Wages are "+MonthlyWage);
-				break;
-			case 2:
-				System.out.println("Part Time Employee is Present");
-				DailyWage = WagePerHr * HrsPerDay_PartTime;
-				System.out.println("Daily Wage is "+DailyWage);
-				MonthlyWage = DailyWage * WorkingDaysPerMonth;
-				System.out.println("Monthly Wages are "+MonthlyWage);
-				break;
-			default:
-				System.out.println("Invalid");
-				break;
+			if(WorkingHrs>100)
+			{
+				DailyWage = 0;
+				MonthlyWage+=DailyWage;
+			}
+			if(WorkingHrs<=100)
+			{
+				double EmpCheck = Math.floor(Math.random() * 10) % 3;
+				int CheckEmp =(int) EmpCheck;//Type casting double to int for Emp attendance check
+
+				switch(CheckEmp)
+				{
+					case 0:
+						System.out.println("Employee is Absent");
+						System.out.println("Wage per day is 0");
+						System.out.println("Total Salary till date is "+MonthlyWage);
+						System.out.println();
+						break;
+					case 1:
+						System.out.println("FullTime Employee is Present");
+						DailyWage = WagePerHr * HrsPerDay_FullTime;
+						System.out.println("Wage per day is "+DailyWage);
+						MonthlyWage = MonthlyWage + DailyWage;
+						WorkingHrs = WorkingHrs + HrsPerDay_FullTime;
+						System.out.println("Total Salary till date is "+MonthlyWage);
+						System.out.println();
+						break;
+					case 2:
+						System.out.println("Part Time Employee is Present");
+						DailyWage = WagePerHr * HrsPerDay_PartTime;
+						System.out.println("Wage per Day is "+DailyWage);
+						MonthlyWage = MonthlyWage + DailyWage;
+						WorkingHrs = WorkingHrs + HrsPerDay_PartTime;
+						System.out.println("Total Salary till date is "+MonthlyWage);
+						System.out.println();
+						break;
+					default:
+						System.out.println("Invalid");
+						break;
+				}
+			}
 		}
+
+		System.out.println("Total Salary for month is " +MonthlyWage);
+		System.out.println("Total Working Hours are " +WorkingHrs);
 	}
 }
