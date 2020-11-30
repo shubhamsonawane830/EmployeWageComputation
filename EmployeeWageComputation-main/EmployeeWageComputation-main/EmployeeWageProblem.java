96f5f2a890d0dca8500f90c68f8a073ffb45d003
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-11-30 20:51:34 +0530

    Added: Part Time Employee with Respective Wages

--- EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
+++ EmployeeWageComputation-main/EmployeeWageComputation-main/EmployeeWageProblem.java
@@ -1,16 +1,32 @@
 public class EmployeeWageProblem{
 	public static void main(String args[]){
-		int WagePerHr=20;
-		int HrsPerDay=8;
+		int is_full_time = 1;
+		int is_part_time = 2;
+
+		int WagePerHr = 20;
+		int HrsPerDay_FullTime = 8;
+		int HrsPerDay_PartTime = 4;
 		int DailyWage;
+
 		System.out.println("*****WELCOME TO EMPLOYEE WAGE PROBLEM*****");
-		double EmpCheck = Math.floor(Math.random() * 10) % 2;
-		if(EmpCheck == 1){
-			System.out.println("Employee is Present");
-			DailyWage = WagePerHr*HrsPerDay;
+
+		double EmpCheck = Math.floor(Math.random() * 10) % 3;
+
+		if(EmpCheck == 1)
+		{
+			System.out.println("Full Time Employee is Present");
+			DailyWage = WagePerHr * HrsPerDay_FullTime;
 			System.out.println("Daily Wage is "+DailyWage);
-		}else{
-			System.out.println("Employee is absent");
+		}
+		else if(EmpCheck ==2)
+		{
+			System.out.println("Part Time Employee is Present");
+			DailyWage = WagePerHr * HrsPerDay_PartTime;
+			System.out.println("Daily Wage is "+DailyWage);
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
 		}
 	}
 }
